afed0db07b377105fe07cdd4fe6b82ecfe16d9eb
Rainer Simon <aboutgeo@gmail.com>
2022-05-09 17:41:06 +0200

    Added missing BaseLaysers file

---
+++ src/map/BaseLayers.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Source, Layer } from 'react-map-gl';
+
+import { geojsonLineStyle } from './styles/BackgroundLayers';
+
+const parseRasterConfig = config => (
+  <Source
+    key={config.name}
+    type="raster"
+    scheme={config.scheme}
+    tiles={config.tiles}
+    tileSize={config.tileSize}
+    attribution={config.attribution}>
+
+    <Layer
+      type="raster"
+      minzoom={config.minzoom || 0}
+      maxzoom={config.maxzoom  || 24} />
+  </Source>
+)
+
+const parseGeoJSONConfig = config => (
+  <Source 
+    key={config.name}
+    type="geojson" 
+    data={config.src}>
+
+    <Layer
+      {...geojsonLineStyle(config.color)} />
+
+  </Source>
+)
+
+export const parseLayerConfig = config => {
+  console.log(config);
+  if (config.type === 'raster') {
+    return parseRasterConfig(config);
+  } else if (config.type === 'geojson') {
+    return parseGeoJSONConfig(config);
+  }
+}
