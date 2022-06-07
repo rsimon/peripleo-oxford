56a142e739f248d86c9033805218dd5036e34eb4
Rainer Simon <aboutgeo@gmail.com>
2022-06-07 08:26:28 +0200

    Range bucket computation, first proof-of concept

--- src/state/search/metric/RangeFacet.js
+++ src/state/search/metric/RangeFacet.js
@@ -8,21 +8,32 @@ export default class RangeFacet {
   } 
 
   computeFacetDistribution = (items, postFilter) => {
-    console.log('computing range facet distribution');
+    const bucketLabels = this.buckets.map(b => `${b[0]}-${b[1]}`);
+    const counts = bucketLabels.map(label => ([ label, 0]));
 
     const facetedItems = items.map(item => {
+      const value = item.properties[this.property] || 0;
+      
+      let bucketIdx = this.buckets.findIndex(b => value >= b[0] && value < b[1]); 
+      if (bucketIdx === -1)
+        bucketIdx = this.buckets.length - 1;
+    
+      const label = bucketLabels[bucketIdx];
+
+      counts[bucketIdx][1]++;
 
       return {
         ...item,
         _facet: {
-          name: this.name
+          name: this.name,
+          values: [ label ]
         }
       };
     });
 
     return {
       facet: this.name,
-      counts: [], 
+      counts,
       items: facetedItems
     };
   }
