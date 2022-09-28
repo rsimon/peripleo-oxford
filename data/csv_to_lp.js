afcc0c4807750efa8a4f28b6e64d4eba34a925f0
Rainer Simon <aboutgeo@gmail.com>
2022-09-28 09:52:14 +0200

    Added international example data + support for multiple WAVs per location

--- data/csv_to_lp.js
+++ data/csv_to_lp.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import Papa from 'papaparse';
 import convert from 'geo-coordinates-parser';
 
-const csv = fs.readFileSync('./sample_data_202206.csv', { encoding: 'utf8', flag: 'r' });
+const csv = fs.readFileSync('./sample_data_202209.csv', { encoding: 'utf8', flag: 'r' });
 const sourceData = Papa.parse(csv, { header: true });
 
 const traces = sourceData.data.reduce((traces, record) => {
@@ -14,7 +14,7 @@ const traces = sourceData.data.reduce((traces, record) => {
   
   const { decimalLatitude, decimalLongitude } = convert(record['geo-coordinates']);
 
-  const toTrace = (when, label, audio, audioWhen) => {
+  const toTrace = (when, label, audio) => {
     const tc = parseInt(record[`${label} TC`]) || 0;
     const gc = parseInt(record[`${label} GC`]) || 0;
     const ac = parseInt(record[`${label} AC`]) || 0;
@@ -51,26 +51,37 @@ const traces = sourceData.data.reduce((traces, record) => {
       }
     };
 
-    if (audio && audioWhen === label) {
-      trace.audio = [ audio ];
+    if (audio?.length > 0) {
+      trace.audio = audio;
       trace.properties.audio = 1;
     }
 
     return trace;
   };
 
-  let [ audio, audioWhen ] = record['recording link (period)'].split(' (');
-  if (audio) {
-    audio = audio.trim();
-    audioWhen = audioWhen?.replace(')', '').trim() || '2000s';
-  }
+  const audio = {}; // year, [ audios ]
+
+  const audioRecords = record['recording link (period)'].split('|')?.map(str => str.trim()) || [];
+  audioRecords.forEach(str => {
+    let [ wav, year ] = record['recording link (period)'].split(' (');
+    wav = wav.trim();
+    year = year?.replace(')', '').trim() || '2000s';
+
+    if (wav) {
+      if (audio[year])
+        audio[year].push(wav);
+      else 
+        audio[year] = [ wav ]; 
+    }
+  });
+
 
   const nextTraces = [
-    toTrace(1901, '1901', audio, audioWhen),
-    toTrace(1946, '1946', audio, audioWhen),
-    toTrace(1960, '1960', audio, audioWhen),
-    toTrace(1973, '1973', audio, audioWhen),
-    toTrace(2000, '2000s', audio, audioWhen)
+    toTrace(1901, '1901', audio['1901']),
+    toTrace(1946, '1946', audio['1946']),
+    toTrace(1960, '1960', audio['1960']),
+    toTrace(1973, '1973', audio['1973']),
+    toTrace(2000, '2000s', audio['2000s'])
   ];
   
   return [...traces, ...nextTraces];
